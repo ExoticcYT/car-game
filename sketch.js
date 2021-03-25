37f0aead87d3aac3c305688428fa04a244c9b883
ExoticcYT <approxap@gmail.com>
2021-03-25 16:08:04 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount = 0 ;
+var allPlayers;
+var distance = 0;
+var database;
+var car1, car2, car3, car4, cars;
+var track, car1_img, car2_img, car3_img, car4_img, ground;
+var form, player, game;
+
+function preload(){
+  track = loadImage("../images/track.jpg");
+  car1_img = loadImage("../images/car1.png");
+  car2_img = loadImage("../images/car2.png");
+  car3_img = loadImage("../images/car3.png");
+  car4_img = loadImage("../images/car4.png");
+  ground = loadImage("../images/ground.png");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth-50,displayHeight-50);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+}
+
+//hola senor
+//muy bien, muy bien, not me.
